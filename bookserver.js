35dd198a74c6fb41c9e15c11722ee984464c8aa5
Jim Smith <jim.smith@digital.trade.gov.uk>
2020-07-23 16:46:50 +0100

    First attempt

---
+++ bookserver.js
@@ -0,0 +1,58 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { buildFederatedSchema } = require('@apollo/federation');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+    # This "Book" type defines the queryable fields for every book in our data source.
+    type Book @key(fields: "title") {
+        title: String
+        author: String
+    }
+
+    # The "Query" type is special: it lists all of the available queries that
+    # clients can execute, along with the return type for each. In this
+    # case, the "books" query returns an array of zero or more Books (defined above).
+    type Query {
+        books: [Book]
+    }
+`
+
+const books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    },
+]
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+    Book: {
+        __resolveReference(book, {getBook}) {
+            return getBook(book.title)
+        }
+    }
+}
+
+const getBook = function(title) {
+    return     {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    }
+}
+
+const server = new ApolloServer({ schema: buildFederatedSchema([{ typeDefs, resolvers }]) });
+
+// The `listen` method launches a web server.
+server.listen({port:3000}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+})
---
+++ canon.js
@@ -0,0 +1,16 @@
+const {ApolloServer} = require("apollo-server")
+const {ApolloGateway} = require("@apollo/gateway")
+
+
+const gateway = new ApolloGateway({
+    serviceList: [
+        {name: "books", url: "http://localhost:3000/graphql"},
+        // List of federation-capable GraphQL endpoints...
+    ]
+})
+
+const server = new ApolloServer({gateway, subscriptions: false})
+
+server.listen().then(({url}) => {
+    console.log(`🚀 Server ready at ${url}`)
+})
